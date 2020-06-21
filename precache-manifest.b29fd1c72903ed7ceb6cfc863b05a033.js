ab15d51a2cf33135db0824f4a0e8f657ec5d4f4a
root <souravmondal0341@gmail.com>
2020-06-21 18:39:14 +0530

    Updates

---
+++ precache-manifest.b29fd1c72903ed7ceb6cfc863b05a033.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d266d38e209a136eacbee8507eabf90c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3111751f6a4d17144e9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cfd57b9e602e835eedf9",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "e3111751f6a4d17144e9",
+    "url": "/static/js/2.520f9423.chunk.js"
+  },
+  {
+    "revision": "0c9d6f6eb73851a26fc721a838b54c80",
+    "url": "/static/js/2.520f9423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd57b9e602e835eedf9",
+    "url": "/static/js/main.f8261bc5.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "696f5d4812021623fd3b88d80076b073",
+    "url": "/static/media/resume.696f5d48.pdf"
+  }
+]);
